25ee85b843f76b36ca27b86dc033bd606088c212
RubenArthuys <ruben.arthuys@gmail.com>
2019-02-26 09:36:17 +0100

    express

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 var express = require('express');
-var socket = require('socket.io');
+var http = require('http');
+var fs = require('fs');
 
-hello
+//Load index.html
+var server = http.createServer(function(req, res) {
+  fs.readFile('./index.html', 'utf-8', function(error, content) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(content);
+  });
+});
+
+//Load socket.io
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+  socket.broadcast.emit('message', socket.pseudo + ' is connected');
+
+  socket.on('new', function(pseudo) {
+    socket.pseudo = pseudo;
+  });
+});
+
+
+server.listen(8080);
